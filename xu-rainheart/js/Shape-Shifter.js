c91aec5a5e3d33c310f0a16e4fcf5312ff7ca6c6
ninhua <mailninhua@163.com>
2025-03-21 18:10:02 +0800

    shape参数自定义

--- xu-rainheart/js/Shape-Shifter.js
+++ xu-rainheart/js/Shape-Shifter.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||#rain|亲爱的|生日快乐|我爱你|❤||#heart|');
+      S.UI.simulate('');
     }
 
     S.Drawing.loop(function () {
